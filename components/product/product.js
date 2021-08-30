c2eb2d3443f183414601e7441f208a5665582274
Gafur Iusupaliev <igafurp@gmail.com>
2021-08-30 13:56:39 -0400

    Product Card View Complete

--- components/product/product.js
+++ components/product/product.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./Product.module.css";
+import Link from "next/link";
 
 export default function Product() {
   useEffect(async () => {
@@ -15,25 +16,27 @@ export default function Product() {
     <div className={styles.products}>
       {data ? (
         data.map(({ id, rating, image, title }) => (
-          <div key={id}>
-            <section className={styles.productCard}>
-              <section className={styles.rating}>
-                {rating.rate} ({rating.count} reviews)
+          <Link href="/" key={id}>
+            <a>
+              <section className={styles.productCard}>
+                <Image
+                  className={styles.image}
+                  src={image}
+                  alt={title}
+                  width="300"
+                  height="300px"
+                />
+                <h1 className={styles.title}>
+                  {title.split(" ").map((word, index) => {
+                    if (index < 5) return `${word} `;
+                  })}
+                </h1>
+                <section className={styles.rating}>
+                  {rating.rate} ({rating.count} reviews)
+                </section>
               </section>
-              <Image
-                className={styles.image}
-                src={image}
-                alt={title}
-                width="300"
-                height="300px"
-              />
-              <h1 className={styles.title}>{title}</h1>
-            </section>
-            <section className={styles.purchaseButtons}>
-              <button>Add To Cart</button>
-              <button>Buy Now</button>
-            </section>
-          </div>
+            </a>
+          </Link>
         ))
       ) : (
         <h1>loading</h1>
