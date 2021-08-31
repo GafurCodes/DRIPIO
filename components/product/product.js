96be71dca43d46f1a4d74df6aa0996a093f7b54c
Gafur Iusupaliev <igafurp@gmail.com>
2021-08-31 19:12:27 -0400

    Separated product view from its logic

--- components/product/product.js
+++ components/product/product.js
@@ -3,11 +3,10 @@ import Image from "next/image";
 import styles from "./Product.module.css";
 import Link from "next/link";
 import ReactStars from "react-rating-stars-component";
-
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function Product() {
+export default function Product({ rating, image, title }) {
   useEffect(async () => {
     const res = await fetch("https://fakestoreapi.com/products/");
     const data = await res.json();
@@ -29,45 +28,40 @@ export default function Product() {
 
   return (
     <div className={styles.products}>
-      {data ? (
-        data.map(({ id, rating, image, title }) => (
-          <Link href="/" key={id}>
-            <a>
-              <section className={styles.productCard}>
-                <Image
-                  className={styles.image}
-                  src={image}
-                  alt={title}
-                  width="300"
-                  height="300px"
-                />
-                <h1 className={styles.title}>
-                  {title.split(" ").map((word, index) => {
-                    if (index < 5) return `${word} `;
-                  })}
-                </h1>
-                <section className={styles.rating}>
-                  <ReactStars
-                    size={20}
-                    value={rating.rate}
-                    edit={false}
-                    isHalf={true}
-                  />
-                  <span>{rating.rate} </span>
-                  <span>({rating.count} reviews)</span>
-                </section>
+      <Link href="/">
+        <a>
+          <section className={styles.productCard}>
+            <Image
+              className={styles.image}
+              src={image}
+              alt={title}
+              width="300"
+              height="300px"
+            />
+            <h1 className={styles.title}>
+              {title.split(" ").map((word, index) => {
+                if (index < 5) return `${word} `;
+              })}
+            </h1>
+            <section className={styles.rating}>
+              <ReactStars
+                size={20}
+                value={rating.rate}
+                edit={false}
+                isHalf={true}
+              />
+              <span>{rating.rate} </span>
+              <span>({rating.count} reviews)</span>
+            </section>
 
-                <button className={styles.addToCart} onClick={notify}>
-                  <span>Add to cart</span>
-                  {/* <span> icon</span> */}
-                </button>
-              </section>
-            </a>
-          </Link>
-        ))
-      ) : (
-        <h1>loading</h1>
-      )}
+            <button className={styles.addToCart} onClick={notify}>
+              <span>Add to cart</span>
+              {/* <span> icon</span> */}
+            </button>
+          </section>
+        </a>
+      </Link>
+
       <ToastContainer />
     </div>
   );
