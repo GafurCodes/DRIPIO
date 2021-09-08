b175eda48d0036be4322c556d56c44b1b2786349
Gafur Iusupaliev <igafurp@gmail.com>
2021-09-07 20:00:20 -0400

    Detailed Product View Complete (Including Responsive Design)

--- components/productDetails/productDetails.js
+++ components/productDetails/productDetails.js
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import ItemQuantity from "../itemQuantity/itemQuantity";
+import styles from "./productDetails.module.css";
+import ReactStars from "react-rating-stars-component";
+import { useState } from "react";
 
 export default function ProductDetails({
   title,
@@ -9,15 +13,48 @@ export default function ProductDetails({
   rate,
   rateCount,
 }) {
+  const [itemQuantity, setItemQuantity] = useState(1);
+
+  const getItemQuantity = (itemQ) => {
+    setItemQuantity(itemQ);
+  };
+
   return (
-    <>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-      <h3>{price}</h3>
-      <h3>{category}</h3>
-      <Image src={image} alt={title} width="700px" height="700px" />
-      <h3>{rate}</h3>
-      <h3>{rateCount}</h3>
-    </>
+    <div className={styles.wrapper}>
+      <div className={styles.product}>
+        <section className={styles.text}>
+          <h1 className={styles.title}>{title}</h1>
+          <h3 className={`${styles.thinText} ${styles.category}`}>
+            {category}
+          </h3>
+          <h2 className={styles.description}>{description}</h2>
+          <section className={styles.ratingAndPrice}>
+            <section className={styles.rating}>
+              <section className={styles.stars}>
+                <ReactStars size={20} value={rate} edit={false} isHalf={true} />
+                <h3 className={`${styles.thinText} ${styles.rate}`}>{rate}</h3>
+              </section>
+              <h3 className={`${styles.thinText} ${styles.rateCount}`}>
+                ({rateCount} reviews)
+              </h3>
+            </section>
+            <h3 className={`${styles.thinText} ${styles.price}`}>
+              ${price * itemQuantity}
+            </h3>
+          </section>
+          <section className={styles.controls}>
+            <ItemQuantity passQuantity={getItemQuantity} />
+            <button>Add To Cart</button>
+          </section>
+        </section>
+        <Image
+          src={image}
+          alt={title}
+          width="400px"
+          height="400px"
+          className={styles.productImage}
+        />
+      </div>
+    </div>
   );
 }
