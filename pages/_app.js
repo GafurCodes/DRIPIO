a83f3256ad3f2e44349262d42020a10cf04ef65b
Gafur Iusupaliev <igafurp@gmail.com>
2021-09-12 16:00:50 -0400

    Modified chakraui theme

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,18 @@ import { useState } from "react";
 import { CartContext } from "../components/cartContext/cartContext";
 import Cart from "../components/cart/cart";
 
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      primary: "#08d9d6",
+      bg: "#252a34",
+      tertiary: "#eaeaea",
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   const [cart, setCart] = useState([]);
 
@@ -12,10 +24,8 @@ function MyApp({ Component, pageProps }) {
     setCart((prevCart) => [...prevCart, { item, quantity }]);
   };
 
-  console.log(cart);
-
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <CartContext.Provider value={cart}>
         <Layout>
           <Cart />
